2dd91b5558ae4ccf4ecd09bf8ab5bdd110f36622
ali97 <muhammad.ali@pexelsoft.com>
2024-08-29 17:30:12 +0500

    project started

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        &copy; Cpoyright by ByteGrad. Intended for Learning or for Portfolio
+      </small>
+      <p>Not allowed to use as your own teaching material.</p>
+    </footer>
+  );
+}
